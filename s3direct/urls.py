a102161be039f8edf2f4b612506afe9cedf64b03
Arun Prabhakar <arun.pbk@stayq.com>
2022-01-26 12:25:03 +0530

    url => re_path

--- s3direct/urls.py
+++ s3direct/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import re_path
 from s3direct.views import get_upload_params, generate_aws_v4_signature
 
 urlpatterns = [
-    url('^get_upload_params/', get_upload_params, name='s3direct'),
-    url('^get_aws_v4_signature/',
+    re_path('^get_upload_params/', get_upload_params, name='s3direct'),
+    re_path('^get_aws_v4_signature/',
         generate_aws_v4_signature,
         name='s3direct-signing'),
 ]
